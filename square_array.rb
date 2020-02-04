81031791750c6be67e3fcac9f74746fcf69c2cf6
Nathan Bohn <nathantbohn@gmail.com>
2020-02-04 01:11:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
   count = 0; 
-  newArray 
+  newArray = []
   while count < array.size
        newArray[count] = (array[count]*array[count])
        count+= 1 
